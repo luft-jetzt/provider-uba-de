b71bc25d42c4938cc45a7df59652d7077374a05f
Malte Hübner <maltehuebner@gmx.org>
2018-11-02 18:30:22 +0100

    Broken WIP.

--- SourceFetcher/Reporting/1SMWMAX.php
+++ SourceFetcher/Reporting/1SMWMAX.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
-namespace App\SourceFetcher\Reporting;
+namespace App\Provider\UmweltbundesamtDe\Reporting;
 
 /**
- * Acht-Stunden-Tagesmaxima
+ * Ein-Stunden-Tagesmaxima
  */
-class Ub8SMWMAX extends AbstractUbReporting
+class SMWMAX extends AbstractReporting
 {
     public function __construct(\DateTimeImmutable $dateTime)
     {
