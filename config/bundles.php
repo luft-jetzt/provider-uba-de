119b71891de4cc67121d9664f77de4165ecc62f0
Malte Hübner <maltehuebner@gmx.org>
2024-07-03 17:44:37 +0200

    Update dependencies.

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Caldera\LuftApiBundle\LuftApiBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
